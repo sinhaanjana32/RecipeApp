5c78ac4298c48250e7465dcd5a342cee0e569809
Anjana <sinhaanjana32@gmail.com>
2020-06-30 15:02:00 +0300

    Updates

---
+++ precache-manifest.0214d922aa4ece2608cac218e37afc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1268490f8e4f86e9536bf3fd801e726",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "792b87bb2277e418a423",
+    "url": "/RecipeApp/static/css/main.78748214.chunk.css"
+  },
+  {
+    "revision": "7753739649a2938da221",
+    "url": "/RecipeApp/static/js/2.3c795e18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.3c795e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792b87bb2277e418a423",
+    "url": "/RecipeApp/static/js/main.5e5483d8.chunk.js"
+  },
+  {
+    "revision": "db1386236e9e8bdc3314",
+    "url": "/RecipeApp/static/js/runtime-main.aa5bbe4b.js"
+  }
+]);
